b43bded1f8a66ed6b6b0434f852573439fa95bab
whitelotus19 <centmarde.campado@gmail.com>
2024-11-05 20:35:40 +0800

    adding mana mechanics.

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,39 @@
 <template>
   <v-app>
     <v-main>
-      <router-view />
+      <div v-if="isLoading" class="loading-screen">
+        <v-progress-circular indeterminate color="primary"></v-progress-circular>
+        <p>Loading...</p>
+      </div>
+      <router-view v-else />
     </v-main>
   </v-app>
 </template>
 
 <script setup>
-  //
-</script>
-<style>
+import { ref, onMounted } from 'vue';
 
+const isLoading = ref(true);
+
+// Simulate a loading process
+onMounted(() => {
+  // Simulating an asynchronous operation
+  setTimeout(() => {
+    isLoading.value = false; // Set loading to false after loading is complete
+  }, 2000); // Adjust the duration as needed
+});
+</script>
+
+<style>
+.loading-screen {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: #e6d011;
+  position: absolute;
+  width: 100%;
+  z-index: 999; /* Ensure the loading screen is above other elements */
+}
 </style>
