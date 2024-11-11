ce9eda0e0215eba76c64aeeca1997f02186317a8
Shinjitsu <71762653+shinjitsue@users.noreply.github.com>
2024-11-12 01:16:33 +0800

    style: 🎄 adjust margin and add glass background effect

--- src/components/CharacterDisplayAi.vue
+++ src/components/CharacterDisplayAi.vue
@@ -346,7 +346,11 @@ onBeforeUnmount(() => {
 .tilt-card {
   transform: perspective(1000px) rotateY(18deg); /* Adjust the degree as needed */
   transition: transform 0.3s ease-in-out;
-  background-color: #00000048;
+  background-color: rgba(0, 0, 0, 0.313);
+  box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+  backdrop-filter: blur(3px);
+  -webkit-backdrop-filter: blur(3.4px);
+  border: 1px solid rgba(0, 0, 0, 0.06);
   z-index: 999;
 }
 
@@ -382,6 +386,7 @@ onBeforeUnmount(() => {
   width: 27rem; /* Adjust the width as needed */
   height: auto; /* Maintain aspect ratio */
   margin-top: 8rem;
+  margin-right: 5rem;
 }
 
 .animated-progress {
